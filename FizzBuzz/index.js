f4012b3cc0f7711ace016e3af4e7c84accc10d18
zoink506 <benhubble03@gmail.com>
2022-11-20 16:37:11 +1100

    Print fizzbuzz array to DOM

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -1,14 +1,42 @@
 // FizzBuzz
-for(let i = 1; i <= 100; i++) {
+const body = document.body;
 
-  if(i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
-  } else if(i % 3 == 0) {
-    console.log("Fizz");
-  } else if(i % 5 == 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
+function createArray() {
+  arr = new Array();
+
+  for(let i = 1; i <= 100; i++) {
+    if(i % 3 == 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if(i % 3 === 0) {
+      arr.push("Fizz");
+    } else if(i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
   }
 
+  return arr;
 }
+
+function printArrayToDOM(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let div = document.createElement("div");
+
+    if(arr[i] === "FizzBuzz") {
+      div.setAttribute("class", "fizz-buzz");
+    } else if(arr[i] === "Fizz") {
+      div.setAttribute("class", "fizz");
+    } else if(arr[i] === "Buzz") {
+      div.setAttribute("class", "buzz");
+    } else {
+      div.setAttribute("class", "none");
+    }
+
+    div.innerText = arr[i].toString();
+    body.append(div);
+  }
+}
+
+const fizzBuzzArr = createArray();
+printArrayToDOM(fizzBuzzArr);
